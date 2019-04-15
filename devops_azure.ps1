c19e6819ef27180d93f07745174dea6e3ac3704e
Sebastian Jaschinski <sebastian.jaschinski@outlook.com>
2019-04-15 10:52:57 +0200

    Removed WSL for Windows Server 2016

--- devops_azure.ps1
+++ devops_azure.ps1
@@ -31,8 +31,8 @@ executeScript "Browsers.ps1";
 
 executeScript "HyperV.ps1";
 RefreshEnv
-executeScript "WSL.ps1";
-RefreshEnv
+#executeScript "WSL.ps1";
+#RefreshEnv
 executeScript "Docker.ps1";
 
 choco install -y powershell-core
@@ -42,8 +42,8 @@ choco install -y microsoftazurestorageexplorer
 choco install -y terraform
 
 # Install tools in WSL instance
-write-host "Installing tools inside the WSL distro..."
-Ubuntu1804 run apt install ansible -y
+#write-host "Installing tools inside the WSL distro..."
+#Ubuntu1804 run apt install ansible -y
 
 Enable-UAC
 Enable-MicrosoftUpdate
